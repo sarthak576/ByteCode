90bcbad2061ba75dca795ef9700fc4bc00ac4a11
sarthak576 <sarthakguptarjn@gmail.com>
2025-05-24 07:12:55 +0530

    sign in page work in progress 80% done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import SignIn from "./components/Signin";
 
 function App() {
   return (
-    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+   
 <SignIn/>
-    </Box>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = "6869876952-nrkehi1pqe7bcecbhpcsfcfsiem7nc2p.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <GoogleOAuthProvider clientId={clientId}>
+  
       <App />
-    </ChakraProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
